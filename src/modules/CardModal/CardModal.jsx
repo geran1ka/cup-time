f1a719a04fc135468a3214c71bc0509d56fa40d2
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-09-04 23:29:37 +0300

    finally project

--- src/modules/CardModal/CardModal.jsx
+++ src/modules/CardModal/CardModal.jsx
@@ -1,18 +1,8 @@
+import s from "./CardModal.module.scss";
 import Modal from "react-modal";
 import { API_URL } from "../../const";
 import { useState } from "react";
 import { useCart } from "../../context/CartContext";
-
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
 
 Modal.setAppElement("#root");
 
@@ -41,28 +31,122 @@ export const CardModal = ({ isOpen, onRequestClose, data }) => {
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-      style={customStyles}
-      contentLabel="Card Modal"
+      className={s.modal}
+      overlayClassName={s.overlay}
+      contentLabel={data.title}
     >
-      <h2>{data.title}</h2>
-      <img src={`${API_URL}${data.img}`} alt={data.title} />
-      <p>{data.price}</p>
-      <ul>
-        {Object.entries(data.additional).map(([key, value]) => (
-          <li key={key}>
-            <strong>{key}:</strong> {value}
-          </li>
-        ))}
-      </ul>
+      <img className={s.image} src={`${API_URL}${data.img}`} alt={data.title} />
 
-      <div>
-        <button onClick={handleDecrease}>-</button>
-        <input type="number" value={quantity} readOnly />
-        <button onClick={handleEncrease}>+</button>
+      <div className={s.content}>
+        <div className={s.header}>
+          <h2 className={s.title}>
+            {data.title}({data.id})
+          </h2>
+
+          <p className={s.price}>{data.price}&nbsp;₽</p>
+        </div>
+
+        <ul className={s.list}>
+          {Object.entries(data.additional).map(([key, value]) => (
+            <li key={key} className={s.item}>
+              <span className={s.field}>{key}:</span>
+              <span className={s.value}>{value}</span>
+            </li>
+          ))}
+        </ul>
+
+        <div className={s.footer}>
+          <div className={s.count}>
+            <button className={s.btn} onClick={handleDecrease}>
+              <svg
+                width="36"
+                height="36"
+                viewBox="0 0 36 36"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="0.5"
+                  y="0.5"
+                  width="35"
+                  height="35"
+                  rx="3.5"
+                  stroke="#B8B8B8"
+                />
+                <rect x="12" y="17" width="12" height="2" fill="#1D1C1D" />
+              </svg>
+            </button>
+            <input
+              className={s.number}
+              type="number"
+              value={quantity}
+              readOnly
+            />
+            <button className={s.btn} onClick={handleEncrease}>
+              <svg
+                width="36"
+                height="36"
+                viewBox="0 0 36 36"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="0.5"
+                  y="0.5"
+                  width="35"
+                  height="35"
+                  rx="3.5"
+                  stroke="#B8B8B8"
+                />
+                <rect x="12" y="17.25" width="12" height="1.5" fill="#1D1C1D" />
+                <rect
+                  x="17.25"
+                  y="24"
+                  width="12"
+                  height="1.5"
+                  transform="rotate(-90 17.25 24)"
+                  fill="#1D1C1D"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <button className={s.btnAddCart} onClick={handleAddToCart}>
+            Добавить
+          </button>
+        </div>
       </div>
 
-      <button onClick={handleAddToCart}>Добавить в корзину</button>
-      <button onClick={onRequestClose}>Закрыть</button>
+      <button
+        className={s.btnCloseCard}
+        onClick={onRequestClose}
+        aria-label={`Закрыть модальное окно с товаром: ${data.title}`}
+      >
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="5.71228"
+            y="14.1975"
+            width="12"
+            height="1.5"
+            transform="rotate(-45 5.71228 14.1975)"
+            fill="#B8B8B8"
+          />
+          <rect
+            x="14.1976"
+            y="15.2582"
+            width="12"
+            height="1.5"
+            transform="rotate(-135 14.1976 15.2582)"
+            fill="#B8B8B8"
+          />
+        </svg>
+      </button>
     </Modal>
   );
 };
--- src/modules/Hero/Hero.jsx
+++ src/modules/Hero/Hero.jsx
@@ -1,17 +1,20 @@
 import classNames from "classnames";
 import s from "./Hero.module.scss";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export const Hero = () => {
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category");
   return (
     <section className={s.hero}>
       <div className={classNames("container", s.hero__container)}>
         <div className={s.hero__wrapper}>
           <h1 className={s.hero__title}>Попробуй новый вкус Арабики</h1>
-
-          <Link className={s.hero__link} to="/products?category=coffee">
-            Перейти к кофе
-          </Link>
+          {category !== "coffee" ? (
+            <Link className={s.hero__link} to="/products?category=coffee">
+              Перейти к кофе
+            </Link>
+          ) : null}
         </div>
       </div>
     </section>
